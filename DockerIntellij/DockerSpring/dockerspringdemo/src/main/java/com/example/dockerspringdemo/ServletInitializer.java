6c55ebe8ab2d85657a4a779fc1284cd21b7b71b5
Sirmisa <misael.pinedo@hotmail.com>
2019-11-11 22:30:25 -0600

    Factory implementation
    (cherry picked from commit 7bb4ee9d2f9f0223e2b3f882ca5aca4c082f4b2b)

---
+++ DockerIntellij/DockerSpring/dockerspringdemo/src/main/java/com/example/dockerspringdemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DockerspringdemoApplication.class);
+    }
+
+}
